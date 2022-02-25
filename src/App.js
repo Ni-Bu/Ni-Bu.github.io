b02dfe60d7a3626571aee65993a6914a58927256
Muhammad Ahmad <muhammadahmad9497@hotmail.com>
2022-02-25 18:49:06 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import { Parallax } from "react-parallax";
+
+function App() {
+  return (
+    <div className="App">
+      <Parallax>
+        <div>HTML INSIDE THE PARALLAX</div>
+      </Parallax>
+    </div>
+  );
+}
+
+export default App;
